48ac572a76c5ccd76cf6cae2338d477410b8fca6
Nikita Grichan <anivire.work@gmail.com>
2024-06-28 00:37:14 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,7 @@ export const app = new Elysia()
   )
   .use(
     cors({
-      // origin: !IS_DEV ? /(.*\.)?dotcreators\.xyz$/ : true,
-      origin: 'https://www.dotcreators.xyz',
+      origin: !IS_DEV ? /(.*\.)?dotcreators\.xyz$/ : true,
       methods: ['GET', 'POST', 'PATCH'],
       allowedHeaders: ['Content-Type', 'Authorization'],
       maxAge: 500,
