b941b601b479a8013f7883910f71f880ee262984
Nikita Grichan <anivire.work@gmail.com>
2024-10-19 01:39:26 +0300

    docs: add JSDoc comments

--- src/utils/webhook/DiscordWebhook.ts
+++ src/utils/webhook/DiscordWebhook.ts
@@ -8,12 +8,20 @@ export class DiscordWebhook implements IDiscordWebhook {
   private logHook: Webhook;
   private suggestionHook: Webhook;
 
+  /**
+   * Initialize webhooks for different message types
+   */
   constructor() {
     this.errorHook = new Webhook(webhookConfig.errorWebhookUrl);
     this.logHook = new Webhook(webhookConfig.logWebhookUrl);
     this.suggestionHook = new Webhook(webhookConfig.suggestionWebhookUrl);
   }
 
+  /**
+   * Sends an error message to discord channel binded to hook
+   * @param title The title of the message
+   * @param message The content of the message
+   */
   public sendErrorMessage(title: string, message: string): void {
     const embed = new EmbedBuilder()
       .setColor('#FA4545')
@@ -23,10 +31,15 @@ export class DiscordWebhook implements IDiscordWebhook {
 
     this.errorHook.addEmbed(embed);
     this.errorHook.send().catch(e => {
-      console.log('Error while trying to send discord message: ', e);
+      console.error('Error while trying to send discord message: ', e);
     });
   }
 
+  /**
+   * Sends a log message to discord channel binded to hook
+   * @param title The title of the message
+   * @param message The content of the message
+   */
   public sendLogMessage(title: string, message: string): void {
     const embed = new EmbedBuilder()
       .setColor('#FF902B')
@@ -36,10 +49,17 @@ export class DiscordWebhook implements IDiscordWebhook {
 
     this.logHook.addEmbed(embed);
     this.logHook.send().catch(e => {
-      console.log('Error while trying to send discord message: ', e);
+      console.error('Error while trying to send discord message: ', e);
     });
   }
 
+  /**
+   * Sends an artist suggestion message to discord channel binded to hook
+   * @param username The suggested artist's username
+   * @param avatarUrl The suggested artist's avatar URL
+   * @param tags The suggested artist's tags
+   * @param country The suggested artist's country
+   */
   public sendArtistSuggestion(
     username: string,
     avatarUrl: string,
@@ -71,7 +91,7 @@ export class DiscordWebhook implements IDiscordWebhook {
 
     this.suggestionHook.addEmbed(embed);
     this.suggestionHook.send().catch(e => {
-      console.log('Error while trying to send discord message: ', e);
+      console.error('Error while trying to send discord message: ', e);
     });
   }
 }
