e0075c721eb6cae1ff3940dddf5a36ec8b279c5d
Nikita Grichan <anivire.work@gmail.com>
2024-10-20 06:05:40 +0300

    fix: schema adjusts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { drizzleConfig } from './src/utils/database/drizzle/drizzle.config';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/utils/database/drizzle/schema.ts',
+  schema: './src/utils/database/drizzle/schema',
   dialect: 'postgresql',
   dbCredentials: {
     url: drizzleConfig.DATABASE_CONNECTION_URL,
