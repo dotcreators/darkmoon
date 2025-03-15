8f9caef8be7101af8d8193da18feddd44608b63a
Nikita Grichan <anivire.work@gmail.com>
2025-03-15 23:38:36 +0300

    feat: overall status

--- src/controllers/v2/healthcheck/healthcheck.controller.ts
+++ src/controllers/v2/healthcheck/healthcheck.controller.ts
@@ -5,8 +5,23 @@ const healthcheckRoutes = new Elysia({
   detail: {
     tags: ['v2'],
   },
-}).get('/', async () => {
-  return true;
-});
+})
+  .get('/', async () => {
+    return true;
+  })
+  .get('/status', async () => {
+    const response = await fetch('https://uptime.betterstack.com/api/v2/status-pages/211117', {
+      headers: {
+        Authorization: `Bearer ${process.env.UPTIME_API_TOKEN}`,
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      return { status: data.data.attributes.aggregate_state };
+    }
+
+    return { status: null };
+  });
 
 export { healthcheckRoutes };
